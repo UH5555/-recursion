650d7fd32de2c55477b89a9f040dcfeb168a2342
UH5555 <zhuhao162@163.com>
2022-02-20 12:01:51 +0800

    0220\函数与递归第二节

---
+++ test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//#include "add.h"
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+////最简单的递归
+//int main()
+//{
+//	main();
+//	return 0;
+//}
+
+//void print(int n)//1234
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);//1234
+//	print(num);
+//	//print(1234)
+//	//print((123)4)
+//	//print(((12)3)4)
+//	//print((((1)2)3)4)
+//	return 0;
+//}
+
+//int my_strlen(char* len)
+//{
+//	int count = 0;
+//	while (*len != '\0')
+//	{
+//		len++;
+//		count++;
+//	}
+//	return count;
+//}
+
+int my_strlen(char* len)
+{
+	if (*len != 0)
+		return 1 + my_strlen(len + 1);
+	else
+		return 0;
+}
+
+//大事化小
+//my_strlen("bit");
+//1+my_strlen("it");
+//1+1+my_strlen("t");
+//1+1+1+my_strlen("");
+//1+1+1+0
+
+#include <string.h>
+int main()
+{
+
+	char arr[] = "bit";
+	//int len = strlen(arr);
+	//printf("%d\n", len);
+	int len = my_strlen(arr);
+	printf("len = %d\n", len);
+	return 0;
+}
